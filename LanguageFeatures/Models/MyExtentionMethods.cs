acb54bafe99c694f37d49e6062198f25324d21b4
Vlad Bulaniy <bulaniy.vlad@apriorit.com>
2018-12-26 12:52:49 +0200

    Added extention method by Intetface

--- LanguageFeatures/Models/MyExtentionMethods.cs
+++ LanguageFeatures/Models/MyExtentionMethods.cs
@@ -1,11 +1,13 @@
-﻿namespace LanguageFeatures.Models
+﻿using System.Collections.Generic;
+
+namespace LanguageFeatures.Models
 {
     public static class MyExtentionMethods
     {
-        public static decimal TotalPrices (this ShoppingCart cartParam)
+        public static decimal TotalPrices (this IEnumerable<Product> productEnum)
         {
             decimal total = 0;
-            foreach (Product prod in cartParam.Products)
+            foreach (Product prod in productEnum)
             {
                 total += prod.Price;
             }
