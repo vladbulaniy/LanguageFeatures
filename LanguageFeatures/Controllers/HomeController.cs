6e8123f5473867a1b540db34e204afc843f90176
vladbulanyy <coollife@i.ua>
2019-01-06 21:28:49 +0200

    added using ViewBag object and Razor operation if/foreach

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -91,13 +91,33 @@ namespace LanguageFeatures.Controllers
 
         public ViewResult RazerIndex()
         {
-            
+
             return View(product);
         }
 
         public ActionResult NameAndPrice()
         {
             return View(product);
+        }
+
+        public ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+            return View(product);
+        }
+
+        public ActionResult DemoArray()
+        {
+            Product[] array = {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+             };
+            return View(array);
         }
     }
 }
