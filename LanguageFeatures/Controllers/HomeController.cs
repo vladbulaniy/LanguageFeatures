10cc13ab50eba46294f636d8911c7915b2f52b26
vladbulanyy <coollife@i.ua>
2019-01-06 20:22:07 +0200

    added new view from controller method

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -80,16 +80,23 @@ namespace LanguageFeatures.Controllers
             return View("Result", (object)result.ToString());
         }
 
+        Product product = new Product
+        {
+            ProductId = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersport",
+            Price = 275.67m
+        };
+
         public ViewResult RazerIndex()
         {
-            Product product = new Product
-            {
-                ProductId = 1,
-                Name = "Kayak",
-                Description = "A boat for one person",
-                Category = "Watersport",
-                Price = 275.67m
-            };
+            
+            return View(product);
+        }
+
+        public ActionResult NameAndPrice()
+        {
             return View(product);
         }
     }
